826d77a3e54d2071b94a2b8f46c2529d4ad74c50
Dima Devgru <git@devg.ru>
2018-11-26 07:58:23 +0300

    use separate color-descriptor library

--- app/components/ColorCard.js
+++ app/components/ColorCard.js
@@ -5,43 +5,11 @@ import objectMap from 'object-map';
 import classNames from 'classnames';
 import { hexToRgba } from 'hex-and-rgba'
 import ClosestColor from '../domain/ClosestColor'
+import ColorDescriptor from 'color-descriptor';
+
 const cents = round(0.01);
 
-const RED_HUE = 20;
-const BLUE_HUE = 306.28;
-
-function lightness(color) {
-  const L = color.l;
-
-  return L < 5 ? 'very dark'
-    : L < 35 ? 'dark'
-    : L > 95 ? 'very bright'
-    : L > 65 ? 'bright'
-    : undefined;
-}
-
-function chromatic(color) {
-  const C = color.c;
-
-  return C == 0 ? 'achromatic' : C < 10 ? 'almost achromatic' : 'chromatic';
-}
-
-function temperature(color) {
-  const H = color.h;
-  const C = color.c;
-
-  return C == 0 || H < RED_HUE || H > BLUE_HUE ? undefined : H < 143.95 ? 'warm' : 'cold';
-}
-
-function propertyOf(color) {
-  return property => property(color)
-}
-
-const properties = [
-  lightness,
-  chromatic,
-  temperature
-];
+const descriptor = new ColorDescriptor();
 
 function ColorCard(props) {
   const color = props.color;
@@ -56,7 +24,7 @@ function ColorCard(props) {
 
   const prettyHcl = objectMap(hclColor, cents);
   const prettyRgb = objectMap(rgbColor, cents);
-  if (prettyHcl.c == 0) prettyHcl.h = 'any';
+  if (prettyHcl.c === 0) prettyHcl.h = 'any';
 
   const textClasses = classNames({
     'color-card__text': true,
@@ -64,8 +32,7 @@ function ColorCard(props) {
     'color-card__text_dark': hclColor.l <= 50
   });
 
-  const description = properties.map(propertyOf(prettyHcl)).filter(Boolean).join(', ');
-
+  const description = descriptor.describe(color).join(', ');
   return (
     <div className="color-card" style={cardStyle}>
       <div className={textClasses}>
