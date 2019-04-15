2643b9ef10fbddadeac9eab124945b6272573e87
Dima Devgru <git@devg.ru>
2019-04-15 19:15:08 +0300

    Updates

--- test/test.js
+++
@@ -1,7 +0,0 @@
-import { expect } from 'chai';
-import ClosestColor from '../app/domain/ClosestColor'
-describe('ClosestColor', () => {
-  it('knows white\'s name', () => {
-    expect(ClosestColor('#FFFFFF').name).to.equal('White');
-  });
-});
